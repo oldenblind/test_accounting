790aea264c1f30f5b0b0226fb138b3c9abbf1903
Vladimir Babich <oldenblind@gmail.com>
2022-05-12 15:33:57 +0300

    feat: add error rendering while authenticate user

--- app/commands/cmd/users/sign_in.rb
+++ app/commands/cmd/users/sign_in.rb
@@ -2,8 +2,13 @@ class Cmd::Users::SignIn < Cmd::Base
   string :username
   password :password
 
+  class IncorrectCredentials < StandardError; end
+
   def execute
-    user.authenticate(password) || {}
+    authenticated_user = user.authenticate(password)
+    raise IncorrectCredentials, 'Invalid credentials' unless authenticated_user
+  rescue IncorrectCredentials => e
+    errors.add(:sign_in, e.message)
   end
 
   private
